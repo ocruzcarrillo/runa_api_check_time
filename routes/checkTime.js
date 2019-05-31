45c8ea379eb28cc4477ae4252b1be577b3c2c784
ocruzcarrillo <48920122+ocruzcarrillo@users.noreply.github.com>
2019-05-31 12:45:16 -0500

    Add files via upload

--- routes/checkTime.js
+++ routes/checkTime.js
@@ -3,17 +3,17 @@ var router = express.Router();
 var CheckTime = require('../model/controllers/CheckTime.js');
 
 /* CHECK TIME REGISTERS */
-router.get('/checkTime', function(req, res, next) {
+router.get('/', function(req, res, next) {
   CheckTime.checkTimeAll(req, res);
 });
 
 /* CHECK TIME REGISTER */
-router.post('/checkTime/:id', function(req, res, next) {
+router.post('/:id', function(req, res, next) {
   CheckTime.checkTime(req, res);
 });
 
 /* USER CHECK TIME REGISTERS */
-router.get('/checkTime/:id', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
   CheckTime.checkTimeUser(req, res);
 });
 
